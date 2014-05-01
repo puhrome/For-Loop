28cf27746c19da0e16063945cfe9d18efd16fc8c
puhrome <phirom.yim@fullsail.edu>
2014-05-01 15:42:35 -0400

    For Loop
    Short hand version of the Do While and While Loop

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+ * For Loop Created by puhrome on 5/1/14.
+ */
+var c = 10;
+
+do {//do or run this code inside here
+    console.log(c + ' kegs on the wall');//code will still run before condition is true
+    c--;//decrement it
+}
+while (c > 0);//while or as long as our condition is true
+
+console.log( "For Loop");
+//short hand for the While and Do While Loops
+for (var i = 10; i > 0; i--){
+    console.log(i + ' kegs on the wall')
+}
